c9c49e073c6eec34ef1456ca756cba7ed06bbd0b
Jakub Muszynski <jakubmuszynski2103@gmail.com>
2024-03-26 15:31:19 +0100

    feat: add solved 90-min task

---
+++ client.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+#include <errno.h>
+
+#define QUEUE_PERMISSIONS 0660
+#define BUFFER_SIZE 128
+#define TIMEOUT 100 // Timeout in milliseconds
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <server_queue_name>\n", argv[0]);
+        exit(1);
+    }
+
+    struct mq_attr attr;
+    mqd_t mq_server, mq_client;
+    char buffer[BUFFER_SIZE];
+    char message[BUFFER_SIZE];
+    int num1, num2;
+
+    // Set up the client queue attributes
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = BUFFER_SIZE;
+    attr.mq_curmsgs = 0;
+
+    // Create the client message queue
+    char client_queue_name[20];
+    sprintf(client_queue_name, "/%d", getpid());
+    mq_client = mq_open(client_queue_name, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+
+    // Connect to the server queue
+    mq_server = mq_open(argv[1], O_WRONLY);
+    if (mq_server == (mqd_t)-1) {
+        perror("Client: mq_open (server)");
+        mq_unlink(client_queue_name);
+        exit(1);
+    }
+
+    while (scanf("%d %d", &num1, &num2) != EOF) {
+        sprintf(message, "%d %d %d", getpid(), num1, num2);
+        mq_send(mq_server, message, strlen(message) + 1, 0);
+
+        // Set up timeout for mq_receive
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = TIMEOUT * 1000;  // Convert milliseconds to microseconds
+
+        fd_set readset;
+        FD_ZERO(&readset);
+        FD_SET(mq_client, &readset);
+
+        int activity = select(mq_client + 1, &readset, NULL, NULL, &tv);
+
+        if (activity > 0) {
+            // Message available within timeout
+            if (mq_receive(mq_client, buffer, BUFFER_SIZE, NULL) >= 0) {
+                printf("Result: %s\n", buffer);
+            } else {
+                perror("Client: mq_receive");
+                break;
+            }
+        } else if (activity == 0) {
+            // Timeout occurred
+            printf("Client: Response timeout\n");
+            break;
+        } else {
+            // Error occurred
+            perror("Client: select");
+            break;
+        }
+    }
+
+    mq_close(mq_server);
+    mq_close(mq_client);
+    mq_unlink(client_queue_name);
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <mqueue.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+#include <errno.h>
+
+#define QUEUE_PERMISSIONS 0660
+#define BUFFER_SIZE 128
+
+mqd_t mq_s, mq_d, mq_m;
+
+mqd_t create_queue(const char *name) {
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 100;
+    attr.mq_curmsgs = 0;
+    mqd_t mq = mq_open(name, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+    if (mq == (mqd_t)-1) {
+        perror("Queue creation failed");
+        exit(1);
+    }
+    return mq;
+}
+
+void destroy_queue(mqd_t mq, const char *name) {
+    mq_close(mq);
+    mq_unlink(name);
+}
+
+void process_message(char *buffer, mqd_t mq, const char *operation) {
+    int client_pid, num1, num2;
+    sscanf(buffer, "%d %d %d", &client_pid, &num1, &num2);
+    int result = 0;
+
+    if (strcmp(operation, "sum") == 0) {
+        result = num1 + num2;
+    } else if (strcmp(operation, "div") == 0) {
+        if (num2 != 0) {  // Prevent division by zero
+            result = num1 / num2;
+        } else {
+            result = 0;  // Could also set an error flag or value here
+        }
+    } else if (strcmp(operation, "mod") == 0) {
+        if (num2 != 0) {
+            result = num1 % num2;
+        } else {
+            result = 0;
+        }
+    }
+
+    char client_queue_name[20], response[20];
+    sprintf(client_queue_name, "/%d", client_pid);
+    sprintf(response, "%d", result);
+    mqd_t mq_client = mq_open(client_queue_name, O_WRONLY);
+    if (mq_client != (mqd_t)-1) {
+        mq_send(mq_client, response, strlen(response) + 1, 0);
+        mq_close(mq_client);
+    } else {
+        perror("Server: mq_open (client)");
+    }
+}
+
+void cleanup() {
+    mq_close(mq_s);
+    mq_close(mq_d);
+    mq_close(mq_m);
+
+    char queue_name[20];
+    sprintf(queue_name, "/%d_s", getpid());
+    mq_unlink(queue_name);
+
+    sprintf(queue_name, "/%d_d", getpid());
+    mq_unlink(queue_name);
+
+    sprintf(queue_name, "/%d_m", getpid());
+    mq_unlink(queue_name);
+}
+
+void handle_sigint(int sig) {
+    cleanup();
+    exit(0);
+}
+
+int main() {
+    struct mq_attr attr;
+    char buffer[BUFFER_SIZE];
+    unsigned int sender;
+
+    signal(SIGINT, handle_sigint);
+
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = BUFFER_SIZE;
+    attr.mq_curmsgs = 0;
+
+    pid_t pid = getpid();
+    char queue_name_s[20], queue_name_d[20], queue_name_m[20];
+    sprintf(queue_name_s, "/%d_s", pid);
+    sprintf(queue_name_d, "/%d_d", pid);
+    sprintf(queue_name_m, "/%d_m", pid);
+
+    mq_s = mq_open(queue_name_s, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+    mq_d = mq_open(queue_name_d, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+    mq_m = mq_open(queue_name_m, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+
+    printf("Queues: %s, %s, %s\n", queue_name_s, queue_name_d, queue_name_m);
+
+    fd_set readset;
+    int result, highest_fd = mq_s > mq_d ? mq_s : mq_d;
+    highest_fd = highest_fd > mq_m ? highest_fd : mq_m;
+
+    while (1) {
+        FD_ZERO(&readset);
+        FD_SET(mq_s, &readset);
+        FD_SET(mq_d, &readset);
+        FD_SET(mq_m, &readset);
+
+        result = select(highest_fd + 1, &readset, NULL, NULL, NULL);
+
+        if (result > 0) {
+            if (FD_ISSET(mq_s, &readset)) {
+                if (mq_receive(mq_s, buffer, BUFFER_SIZE, &sender) >= 0) {
+                    process_message(buffer, mq_s, "sum");
+                } else {
+                    perror("Server: mq_receive from sum queue");
+                }
+            }
+            if (FD_ISSET(mq_d, &readset)) {
+                if (mq_receive(mq_d, buffer, BUFFER_SIZE, &sender) >= 0) {
+                    process_message(buffer, mq_d, "div");
+                } else {
+                    perror("Server: mq_receive from div queue");
+                }
+            }
+            if (FD_ISSET(mq_m, &readset)) {
+                if (mq_receive(mq_m, buffer, BUFFER_SIZE, &sender) >= 0) {
+                    process_message(buffer, mq_m, "mod");
+                } else {
+                    perror("Server: mq_receive from mod queue");
+                }
+            }
+        } else if (result == -1 && errno != EINTR) {
+            perror("Server: select");
+            break;
+        }
+    }
+
+    // Clean up and exit
+    cleanup();
+    return 0;
+}
